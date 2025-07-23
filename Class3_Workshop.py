aca70ffa2b606233a16b34c6442aa28edb0c9304
Bun3nuB <147194297+Bun3nuB@users.noreply.github.com>
2025-07-23 15:14:38 +0700

    addMonsterEffect

--- Class3_Workshop.py
+++ Class3_Workshop.py
@@ -1,63 +1,96 @@
-MonsterHP = 0
-Monstername = ""
-SwordDamage = 20
-Axe = 25
-Gun = 45
-MonsterDead = False
 
-print("-----ยินดีต้อนรับนักผจญภัยทุกท่าน-----")
 while True:
+    MonsterDead = False
+    totalDamage = 0
+    Revivecount = 0
+    ChoiceGoblin = False
+    ChoiceDragon = False
+    ChoiceMinotaur = False
+    ChoiceLichKing = False
+    MonsterEffect = ""
+    SwordDamage = 20
+    Axe = 35
+    Gun = 45
+    print("-----ยินดีต้อนรับนักผจญภัยทุกท่าน-----")
     menuChoice = int(input("ท่านจะทำอะไร (1: สู้กับมอนสเตอร์, 2: ออกจากเกม): "))
     if(menuChoice == 1):
         while True:
-            Monsterchoice = int(input("1.มังกร \n2.ราชาก็อบลิน \n3.มิโนทอร์ \n4.ลิชคิง \nท่านต้องการที่จะเผชิญหน้ากับมอนสเตอร์ตัวไหน :"))
+            print("===========เลือกบอส=========== \n 1.มังกร \n ไม่มีความสามารถพิเศษแต่มีพลังชีวิตจำนวนมาก")
+            print("=============================\n2.ราชาก็อบลิน \n คุณถูกขโมยอาวุธปืน")
+            print("=============================\n3.มิโนทอร์ \n หนังที่หนาทำให้ดาเมจลดลง 10 หน่วย")
+            print("=============================\n4.ลิชคิง \n บังคับให้คุณต่อสู้กับมันใน 6 รอบเท่านั้น\n=============================")
+            Monsterchoice = int(input("ท่านต้องการที่จะเผชิญหน้ากับมอนสเตอร์ตัวไหน :"))
             if(Monsterchoice == 1):
                 Monstername = "มังกร"
-                MonsterHP = 200.0
+                MonsterHP = 250.0
+                ChoiceDragon = True
+                MonsterEffect = "ไม่มีความสามารถพิเศษแต่มีพลังชีวิตจำนวนมาก"
                 break
             elif Monsterchoice == 2:
                 Monstername = "ราชาก็อบลิน"
                 MonsterHP = 150.0
+                ChoiceGoblin = True
+                MonsterEffect  = "คุณถูกขโมยอาวุธปืน"
                 break
             elif Monsterchoice == 3:
                 Monstername = "มิโนทอร์"
                 MonsterHP = 175.0
+                ChoiceMinotaur = True
+                MonsterEffect = "หนังที่หนาทำให้ดาเมจลดลง 10 หน่วย"
+                SwordDamage -= 10
+                Axe -= 10
+                Gun -= 10
                 break
             elif Monsterchoice == 4:
                 Monstername = "ลิชคิง"
                 MonsterHP = 145.0
+                ChoiceLichKing = True
+                MonsterEffect = "บังคับให้คุณต่อสู้กับมันใน 6 รอบเท่านั้น"
                 break
             else:
                 print("ใส่ข้อมูลผิดพลาด โปรดลองใหม่")
-        print(f"ท่านพบกับ{Monstername} {Monstername}ตัวนี้มีเลือดทั้งหมด {MonsterHP} หน่วย")
+        print(f"=====Event===== \nท่านพบกับ{Monstername} {Monstername}ตัวนี้มีเลือดทั้งหมด {MonsterHP} หน่วย")
         HitRound = int(input(f"คุณต้องการจะตีกับ{Monstername}กี่รอบ :"))
+        if(ChoiceLichKing):
+            HitRound = 6
+            print("ลิชคิงบังคับให้คุณต่อสู้กับมันใน 6 รอบเท่านั้น")
         print(f"ท่านจะสู้กับ{Monstername} ทั้งหมด{HitRound} รอบ")
         for i in range(1,HitRound+1):
-                Run = input(f"ท่านต้องการที่จะหลบหนีจาก{Monstername}หรือไม่(1 = ใช่ หรือ 2 = ไม่ :)")
+                Run = input(f"=====หลบหนี===== \nท่านต้องการที่จะหลบหนีจาก{Monstername}หรือไม่(1 = ใช่ หรือ 2 = ไม่ :)")
                 if Run == "1":
                     print(f"ท่านได้หลบหนีจาก{Monstername}")
                     break
                 elif Run == "2":
-                    print(f"ท่านเลือกที่จะต่อสู้ยืนหยัดกับ{Monstername} \nรอบที่ {i} \n1.ดาบ(20 ดาเมจ) \n2.ขวาน(25 ดาเมจ) \n3.ปืน(45 ดาเมจ)")
+                    if(ChoiceGoblin == True):
+                        Gun = 0
+                        print(f"=====เลือกอาวุธ=====\n{MonsterEffect}\nท่านเลือกที่จะต่อสู้ยืนหยัดกับ{Monstername} \nรอบที่ {i}\n1.ดาบ(20 ดาเมจ)\n2.ขวาน(35 ดาเมจ)")
+                    else:
+                        print(f"=====เลือกอาวุธ===== \nท่านเลือกที่จะต่อสู้ยืนหยัดกับ{Monstername} \nรอบที่ {i} \n1.ดาบ(20 ดาเมจ) \n2.ขวาน(35 ดาเมจ) \n3.ปืน(45 ดาเมจ)")
+                        if(ChoiceMinotaur):
+                            print("มิโนทอร์หนังหนาตีไม่เข้าจะลดดาเมจที่คุณทำได้ 10 หน่วย")
                     while True:
-                        Weapon = int(input(f"โปรดเลือกอาวุธที่ท่านจะใช้ต่อสู้กับ{Monstername}:)"))
-                        if(Weapon == 1):
+                        Weapon = int(input(f"โปรดเลือกอาวุธที่ท่านจะใช้ต่อสู้กับ{Monstername}:"))
+                        if(Weapon == 1 ):
                             MonsterHP -= SwordDamage
+                            totalDamage += SwordDamage
                             break
                         elif Weapon == 2:
                             MonsterHP -= Axe
+                            totalDamage += Axe
                             break
-                        elif Weapon == 3:
+                        elif Weapon == 3 and ChoiceGoblin == False:
                             MonsterHP -= Gun
+                            totalDamage += Gun
                             break
                         else:
                             print("ใส่ข้อมูลผิดพลาดลองใหม่")
-                    print(f"{Monstername}เหลือเลือกทั้งหมด {MonsterHP} หน่วย")
+                    print(f"{Monstername}เหลือเลือดทั้งหมด {MonsterHP} หน่วย")
                     if(MonsterHP == 0):
                         MonsterDead = True
                         break
-                    if(MonsterHP < 0):
+                    elif MonsterHP < 0:
                         MonsterHP = 20
+                        Revivecount += 1
                         print(f"{Monstername} กลับมามีพลังอีกครั้ง! \nเลือด {Monstername} กลับมาที่ {MonsterHP} หน่วย")
                 else:
                     print("ใส่ข้อมูลผิดพลาด ลองใหม่")
@@ -68,6 +101,10 @@ while True:
             print(f"ท่านหลบหนีจาก {Monstername} สำเร็จ")
         elif(MonsterDead == False):
             print(f"คุณถูก{Monstername}ฆ่าตาย! \nเสียใจด้วยคล้ายหน้าเอาใหม่นะ")
+        fightReport = int(input("ต้องการดูรายงานการต่อสู้หรือไม่ (1 = ใช่, 2 = ไม่): "))
+        if(fightReport == 1):
+            print("=====รายงานการต่อสู้=====")
+            print(f"จำนวนรอบที่สู้: {i}\nมอนสเตอร์ที่คุณต่อสู้: {Monstername}\nความเสียหายทั้งหมดที่คุณทำ: {totalDamage}\nจำนวนครั้งที่มอนสเตอร์ฟื้นคืนชีพ: {Revivecount}")
     elif(menuChoice == 2):
         break
     else:
